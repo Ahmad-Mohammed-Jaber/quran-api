e99768d9dbefbfd0991f4ddab03bc83e28a8cece
Ahmad-Mohammed-Jaber <178525653+Ahmad-Mohammed-Jaber@users.noreply.github.com>
2025-04-06 07:53:01 +0300

    early late initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+const textSrc = []
+const audios = []
+let totalTime = 0
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function playSequentially(index = 0) {
+    if (index >= audios.length) return;
+  
+    const currentAudio = audios[index];
+    currentAudio.play();
+  
+    currentAudio.onended = () => {
+      playSequentially(index + 1);
+    };
+}
+
+async function main() 
+{
+    const res = await fetch('https://api.alquran.cloud/v1/surah/31/editions/ar.minshawi,quran-unicode,en.sahih')
+    const result = await res.json();
+    const data = result.data
+
+    
+    data.slice(1).forEach(obj => 
+    {
+        let ayat = []
+        obj.ayahs.forEach(ayah => 
+        {
+            ayat.push(ayah)
+        });
+        textSrc.push(ayat)
+    });
+
+    let totalTime = 0
+    const audioSrc = data[0].ayahs
+    audioSrc.forEach(async (ayah, index) => 
+    {
+        const audioElement = new Audio(ayah.audio)
+
+        audioElement.preload = "metadata" 
+        audioElement.onloadedmetadata = () =>  
+        {
+            audios[index] = audioElement
+            totalTime += audioElement.duration
+            console.log(totalTime)
+        }
+    })   
+
+    console.log(audios)
+    audios.forEach(audio => 
+    {
+        console.log(audio.duration)
+        sleep(1000).then(() => totalTime += audio.duration) 
+    });
+    
+    const playAyatButton = document.getElementById('playAyatButton')
+    
+    playAyatButton.setAttribute('disabled', '')
+    sleep(1000).then(() => playAyatButton.removeAttribute('disabled'))
+    
+    playAyatButton.addEventListener('click', playAyat)
+    function playAyat() 
+    {
+        playAyatButton.setAttribute('disabled', '')
+        console.log(totalTime * 1000)
+        sleep(totalTime * 1000).then(() => playAyatButton.removeAttribute('disabled'))
+        let prevTime = 0
+        playSequentially(0)
+    }
+
+
+    let br = document.createElement('br')
+    document.body.appendChild(br)
+
+    textSrc.forEach(text => 
+    {
+        text.forEach(ayah => 
+        {
+            let ayahText = document.createElement('p')
+            ayahText.innerText = ayah.text
+            document.body.append(ayahText)
+            br = document.createElement('br')
+            document.body.append(br)
+        });   
+    });
+}
+
+main()
+
