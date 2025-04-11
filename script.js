2d6b9f5c135834836fd2528102576a269ba14221
Ahmad-Mohammed-Jaber <178525653+Ahmad-Mohammed-Jaber@users.noreply.github.com>
2025-04-11 19:40:14 +0300

    cleaned playing audio code, changed font

--- script.js
+++ script.js
@@ -1,95 +1,64 @@
 const textSrc = []
 const audios = []
-let totalTime = 0
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+const playAyatButton = document.getElementById('playAyatButton')
 
-function playSequentially(index = 0) {
-    if (index >= audios.length) return;
-  
-    const currentAudio = audios[index];
-    currentAudio.play();
-  
-    currentAudio.onended = () => {
-      playSequentially(index + 1);
-    };
-}
-
-async function main() 
-{
-    const res = await fetch('https://api.alquran.cloud/v1/surah/16/editions/ar.minshawi,quran-unicode,en.sahih?offset=67&limit=2')
+async function main() {
+    const res = await fetch('https://api.alquran.cloud/v1/surah/16/editions/ar.husary,quran-uthmani-quran-academy,en.sahih?offset=67&limit=2')
     const result = await res.json();
     const data = result.data
 
-    
-    data.slice(1).forEach(obj => 
-    {
-        let ayat = []
-        obj.ayahs.forEach(ayah => 
+    // Audio generation and playing
+
+    const audioSrc = data[0].ayahs
+    audioSrc.forEach(ayah => {        
+        const audioElement = new Audio(ayah.audio)
+        audios.push(audioElement)
+    })
+
+
+    playAyatButton.addEventListener('click', playAyat)
+    function playAyat() {
+        playAyatButton.setAttribute('disabled', '')
+        playSequentially()
+    }
+
+    function playSequentially(index = 0) {
+        if (index == audios.length)
         {
+            playAyatButton.removeAttribute('disabled')
+            return
+        }
+
+        audios[index].play()
+        audios[index].onended = () => playSequentially(index + 1)
+    }
+
+    data.slice(1).forEach(obj => {
+        let ayat = []
+        obj.ayahs.forEach(ayah => {
             ayat.push(ayah)
         });
         textSrc.push(ayat)
     });
 
-    let totalTime = 0
-    const audioSrc = data[0].ayahs
-    audioSrc.forEach(async (ayah, index) => 
-    {
-        const audioElement = new Audio(ayah.audio)
-
-        audioElement.preload = "metadata" 
-        audioElement.onloadedmetadata = () =>  
-        {
-            audios[index] = audioElement
-            totalTime += audioElement.duration
-            console.log(totalTime)
-        }
-    })   
-
-    console.log(audios)
-    audios.forEach(audio => 
-    {
-        totalTime += audio.duration 
-    });
-    
-    const playAyatButton = document.getElementById('playAyatButton')
-    
-    playAyatButton.setAttribute('disabled', '')
-    sleep(1000).then(() => playAyatButton.removeAttribute('disabled'))
-    
-    playAyatButton.addEventListener('click', playAyat)
-    function playAyat() 
-    {
-        playAyatButton.setAttribute('disabled', '')
-        console.log(totalTime * 1000)
-        sleep(totalTime * 1000).then(() => playAyatButton.removeAttribute('disabled'))
-        let prevTime = 0
-        playSequentially()
-    }
-
+    // Text Generation
 
     let br = document.createElement('br')
     document.body.appendChild(br)
 
-    textSrc.forEach((text, index) => 
-    {
-        text.forEach(ayah => 
-        {
+    textSrc.forEach((text, index) => {
+        text.forEach(ayah => {
             let ayahText = document.createElement('p')
             console.log(ayah)
             ayahText.innerText = `${ayah.text} (${ayah.numberInSurah})`
-            if (index === 0)
-            {
+            if (index === 0) {
                 ayahText.classList.add('arabic-text')
             }
             document.body.append(ayahText)
             br = document.createElement('br')
             document.body.append(br)
-        });   
+        });
     });
 }
 
 main()
-
